c6610831066bbe3f6629a72e4ec107facbc7d0b2
pdr3392 <pchenr1que1@gmail.com>
2021-11-08 03:02:31 -0300

    pagination

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,27 @@
 import { SignInButton } from "../SignInButton";
 import styles from "./styles.module.scss";
+import Link from "next/link";
+import { useRouter } from "next/dist/client/router";
+import { ActiveLink } from "../ActiveLink";
 
 export function Header() {
+  const { asPath } = useRouter();
+
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <img src="/images/logo.svg" alt="Ig.news" />
-        <nav>
-          <a className={styles.active} href="#">
-            Home
+        <Link href="/">
+          <a>
+            <img src="/images/logo.svg" alt="Ig.news" />
           </a>
-          <a href="#">Posts</a>
+        </Link>
+        <nav>
+          <ActiveLink activeClassName={styles.active} href="/">
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/posts" prefetch>
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
         <SignInButton />
       </div>
